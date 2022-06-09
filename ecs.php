8c57163b7bf07ea3721cf44c9e14589689a35f47
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-06-09 08:56:18 +0200

    Updated ECS

--- ecs.php
+++ ecs.php
@@ -2,25 +2,13 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php']);
+    $ecsConfig->parallel();
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::SKIP, [
-        '/contao/*',
-        HeaderCommentFixer::class => null,
-        MethodChainingIndentationFixer::class => [
-            '*/DependencyInjection/Configuration.php',
-        ],
-    ]);
-
-    $parameters->set(Option::LINE_ENDING, "\n");
-    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache');
+    $parameters = $ecsConfig->parameters();
+    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_cache');
 };
