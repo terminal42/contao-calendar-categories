492dad8fd4467d236a52d42962c5d2bf117acb74
Lukas Bableck <lukas@bableck.dev>
2025-05-28 07:18:56 +0200

    Contao 5 compatibility

--- contao/languages/de/tl_calendar_category.php
+++ contao/languages/de/tl_calendar_category.php
@@ -2,3 +2,5 @@
 
 $GLOBALS['TL_LANG']['tl_calendar_category']['name'] = ['Name', 'Geben Sie den Namen der Kategorie ein.'];
 $GLOBALS['TL_LANG']['tl_calendar_category']['name_legend'] = 'Name';
+
+$GLOBALS['TL_LANG']['tl_calendar_category']['copyChilds'] = 'Element ID %s inklusive Kindelemente duplizieren';
--- contao/languages/en/tl_calendar_category.php
+++ contao/languages/en/tl_calendar_category.php
@@ -2,3 +2,5 @@
 
 $GLOBALS['TL_LANG']['tl_calendar_category']['name'] = ['Name', 'Please enter the category name.'];
 $GLOBALS['TL_LANG']['tl_calendar_category']['name_legend'] = 'Name';
+
+$GLOBALS['TL_LANG']['tl_calendar_category']['copyChilds'] = 'Duplicate element ID %s including child elements';
